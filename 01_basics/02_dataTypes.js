ecf7e05e575b9b2a8a47f560a30aa12729e3827b
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2023-12-29 22:19:06 +0000

    In this lecture, I learned about the

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all code as new version.
+
+
+//Data Types 
+
+let name="Ayush"
+let age=22
+let isLoggedint=true
+
+
+
+//number => range= 2^53
+//bigint  "We  use this when we have Integer bigger than 2^53"
+//String (i.e Ayush we can store value of String in bothe the ways "Ayush" and 'Ayush')
+//boolean => stores true and false values 
+//null=> standalone value
+// undefined => 
+// symbol=>//to check uniqueness mostly used in React Js
+
+
+
+
+//object
+
+// if we want to some information regarding any data type we can use "typeof" .
+console.log(typeof  "Ayush") // to check type of any data 
+console.log(typeof age ); // to check type of any variable
+console.log(typeof null);// to know about any dataTypes.
