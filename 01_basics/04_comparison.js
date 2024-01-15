45ae72c593b53c26fa964f67801e8d5d39e0ba6a
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-01-15 16:25:01 +0000

    In this I learned about how boolean comparisons work.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+//boolean comparisons returns true or false
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// Try to avoid these type of conversions
+
+// console.log("2">=1);// output=>true
+// console.log(null>0);// output=>false
+// console.log(null>0);// output=>false
+// console.log(null>0);// output=>true
+
+
+/*
+Difference b/w == and ===
+* == isme vs values compare hote hai data types nhi 
+* === ye values + data types ko compare krta hai .
+i.e
+let num = 5;
+let num1=5
+let strNum = "5";
+
+console.log(num == strNum);  //output=>true 
+console.log(num === strNum); //output=> false
+console.log(num === num1); //output=>true
+*/
