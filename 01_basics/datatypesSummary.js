2376a5d6112469e30412d93702bf7175f1073d8d
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-01-19 16:46:08 +0000

    Learned about non-primitive dataTypes

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -21,7 +21,45 @@
 /*
 Non-Primitive(Reference)
 
+Typeof all non-primitive data types are objects
+
 Array,Objects,Function
+
+const superHeroes=["shaktiman","naahraj","doga"] // Arrays
+
+
+let myObj={
+  name:"Ayush",
+  age:21
+} //object
+
+const myFunc=function(){
+ console.log("Hello ")
+}
+myFunc();//Function
+
 */
 
+
 // JavaScript is a dynamically typed language i.e const score =100 ||const score =false  isme variable ka type run time mai decide hota hai issilye js dynamically typed  hai
+
+
+//Quick recap
+
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+       */
