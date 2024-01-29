e415ebb367794c30ce11ec770d3d6c380b60bb04
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-01-29 15:30:17 +0000

    completed the lecture

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,9 +5,12 @@
 
 //creating an object
 
+const mySym =Symbol("key1")
+
 const jsUser = {
     name:"Ayush",
     age:23,
+    [mySym]:"myKey1",//symbol ko access or create dono krne ke liye square bracket mai rkhna hota hai 
     location:"Punjab",
     email:"ayush@gmail.com",
     isLoggedIn:false,
@@ -17,7 +20,30 @@ const jsUser = {
 
 //accessing objects
 
-console.log(jsUser.name,jsUser.email); //1st way
-console.log(jsUser["fullName"],jsUser["email"]);//2nd way
-
+// console.log(jsUser.name,jsUser.email); //1st way
 //2nd comes in role when while declaring object we declared any variable in "double quotation" we can access "double quotation" variable through 2nd way only and with the both way we can access mutiple values togeter
+// console.log(jsUser["fullName"],jsUser["email"]);//2nd way
+
+// console.log(jsUser[mySym]);
+
+
+//updating values in objects 
+jsUser.email="ayushsrivastavta@gmail.com"
+// console.log(jsUser.email);
+
+
+// freezing objects (it doesn't allow us to change values )
+// Object.freeze(jsUser)
+// jsUser.email="ayushsrivastavta@microsoft.com"
+// console.log(jsUser);
+
+jsUser.greetingOne= function(){
+    console.log("Hello js user");
+   }
+
+jsUser.greetingTwo= function(){
+ console.log(`Hello js user ${this.fullName}`);
+}
+console.log(jsUser.greetingOne());
+console.log(jsUser.greetingTwo());
+
