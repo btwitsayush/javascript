dd274933e2d62be8cec3fbe0c2ea9e018957e790
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-02-13 05:50:53 +0000

    In this lecture,I learned about while and do-while loops

---
+++ 05_iterations/while_loop.js
@@ -0,0 +1,19 @@
+//while
+
+let i=0
+while (i<10) {
+    // console.log(`value of i is ${i}`);
+    i++
+}
+
+
+
+//do-while(first we will do any work than check the condition)
+
+let score =1;
+do{
+    console.log(`socre value is ${score}`);
+    score++;
+} while (score<10) ;
+    
+    
