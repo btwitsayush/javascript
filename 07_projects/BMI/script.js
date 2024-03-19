dfd140735685a9c4acb1574c62aa48886da802f0
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-03-19 16:01:22 +0000

    created bmi project

--- 07_projects/BMI/script.js
+++ 07_projects/BMI/script.js
@@ -1,18 +1,30 @@
 
-const form = document.getElementById('calculator');
+  const form = document.querySelector('.calculator');
 
  form.addEventListener('submit',(e)=>{
+    
+    console.log("hi");
 
     e.preventDefault();
 
     const height = parseInt(document.getElementById('height').value);
     const weight = parseInt(document.getElementById('weight').value);
-
+    
     const bmi = weight / ((height * 0.3048) ** 2);
-
+    
     const result = document.getElementById('result');
 
-    result.innerHTML = "Your BMI value is: " + bmi.toFixed(2);
+
+    // const bmiText = document.createTextNode(` ${bmi.toFixed(2)}`);
+
+
+    // result.appendChild(bmiText);
+
+    const span = document.createElement('span');
+
+    span.appendChild(document.createTextNode( `${bmi.toFixed(2)}`));
+
+    result.appendChild(span);
 })
 
 
