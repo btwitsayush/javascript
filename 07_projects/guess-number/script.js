9dae90f2e46bb44652f7ab3f84bd2dc469751494
Ayush Srivastava <93151067+btwitsayush@users.noreply.github.com>
2024-03-24 17:08:32 +0000

    .

--- 07_projects/guess-number/script.js
+++ 07_projects/guess-number/script.js
@@ -2,43 +2,51 @@ const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 console.log(randomNumber);
 
-let count =10;
-
-// created an array to add previous inputs
-const array =[];
-
-const form = document.querySelector('#form');
-
- form.addEventListener('submit',(e)=>{
-
-    e.preventDefault();
-    
-    // getting value from user 
-
-    const guess = parseInt(document.getElementById('number').value);
-    const attempt = document.getElementById('count');
-
-// adding previous guesses to array
-    array.push(guess);
-
-    const previousValue =document.getElementById('previous');
-    
-    // displaying previous value
-    previousValue.innerHTML=array;
-
-    // displaying whether input is correct or not 
-
-    const result = document.getElementById('result'); 
+const userInput = document.querySelector('#number');
+const submit = document.querySelector('#submit');
+const previous= document.querySelector('#submit');
+const count= document.querySelector('#count');
 
 
-    if(guess===randomNumber){
-     result.innerHTML="correct guess";
+
+const para = document.createElement('p');
+
+let previousGuess=[];
+
+let numGuess=1;
+
+let playGame= true;
+
+if(playGame){
+
+
+}
+
+function validateGuess(guess){
      
-    }
-    else{
-        result.innerHTML=" not correct guess";
-       count = count-1;
-        attempt.innerText=count;
-    }
+}
 
-})
+function checkGuess(guess){
+     
+}
+
+function displayGuess(guess){
+     
+}
+
+function displayMessage(message){
+     
+}
+
+function endGame(){
+ 
+    
+}
+
+function newGame(){
+
+
+}
+
+
+
